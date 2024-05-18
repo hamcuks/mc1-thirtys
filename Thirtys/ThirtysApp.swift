cf670641ca0a0e7bc489b38a4cc3a3f5f4b1971d
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-18 09:49:42 +0700

    Initial Commit

---
+++ Thirtys/ThirtysApp.swift
@@ -0,0 +1,20 @@
+//
+//  ThirtysApp.swift
+//  Thirtys
+//
+//  Created by Ivan Nur Ilham Syah on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ThirtysApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
