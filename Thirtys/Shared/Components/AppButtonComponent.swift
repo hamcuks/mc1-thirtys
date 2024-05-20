ca4ced9638b49651f49c55ff8545c9da90d58f11
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-20 13:52:59 +0700

    feat(component): add isDisabled prop for AppButton, create BackButton component

--- Thirtys/Shared/Components/AppButtonComponent.swift
+++ Thirtys/Shared/Components/AppButtonComponent.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct AppButton: View {
     var label: String
+    var isDisabled: Bool = false
+    
     var body: some View {
         Text(label)
             .font(.subheadline)
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             .bold()
             .padding(.vertical, 12)
-            .background(.kAccent)
+            .background(!isDisabled ? .kAccent : .kSecondary)
             .clipShape(RoundedRectangle(cornerRadius: 56))
+    }
+}
+
+struct BackButton: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        Button(action: { dismiss() }) {
+            Image(systemName: "chevron.backward")
+                .foregroundStyle(.kTitleText)
+                .bold()
+                .frame(width: 40, height: 40, alignment: .center)
+                .background(.kAccent)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
     }
 }
 
