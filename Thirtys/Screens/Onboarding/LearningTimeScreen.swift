6c6702c10313c32f1a865c8c4880278c0b5dd800
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-20 16:53:26 +0700

    feat(screen): create step 3 of onboarding screen

---
+++ Thirtys/Screens/Onboarding/LearningTimeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LearningTimeScreen.swift
+//  Thirtys
+//
+//  Created by Ivan Nur Ilham Syah on 20/05/24.
+//
+
+import SwiftUI
+
+struct LearningTimeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningTimeScreen()
+}
--- Thirtys/Shared/Extensions/SampleExtensions.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SampleExtensions.swift
-//  Thirtys
-//
-//  Created by Ivan Nur Ilham Syah on 18/05/24.
-//
-
-import Foundation
