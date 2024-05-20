33b2817d1dd72151998fa5fcafd9a1b586790b33
Bayu Septyan <bayuseptyan925@gmail.com>
2024-05-20 16:53:11 +0700

    change badge

---
+++ Thirtys/Shared/Components/LearningHistoryEmpty.swift
@@ -0,0 +1,18 @@
+//
+//  LearningHistoryEmpty.swift
+//  Thirtys
+//
+//  Created by Bayu Septyan Nur Hidayat on 20/05/24.
+//
+
+import SwiftUI
+
+struct LearningHistoryEmpty: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningHistoryEmpty()
+}
