6efaf038b9149cf8a44fa520543901e971dbd96f
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-20 14:27:07 +0700

    feat(component): create time picker list item component

---
+++ Thirtys/Screens/Onboarding/StepTwoOnboardingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  StepTwoOnboardingScreen.swift
+//  Thirtys
+//
+//  Created by Ivan Nur Ilham Syah on 20/05/24.
+//
+
+import SwiftUI
+
+struct StepTwoOnboardingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepTwoOnboardingScreen()
+}
