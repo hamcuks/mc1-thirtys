a351adbe03e624e4add8e21629669cdcfddc3ef8
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-23 14:59:01 +0700

    feat: adjust color, spacing, code optimization

--- Thirtys/Screens/StreakScreen.swift
+++ Thirtys/Screens/StreakScreen.swift
@@ -17,101 +17,97 @@ struct StreakScreen: View {
     var body: some View {
         NavigationStack{
             ScrollView{
-                
-                
-                ZStack{
-                    Color.kBackground
-                    VStack(alignment: .leading, spacing: 24){
-                        VStack (alignment: .leading){
-                            HStack(spacing: 4) {
-                                Image(systemName: "flame.fill")
-                                    .resizable()
-                                    .frame(width: 46, height: 53)
+                VStack(alignment: .leading, spacing: 24){
+                    VStack (alignment: .leading){
+                        HStack(spacing: 4) {
+                            Image(systemName: "flame.fill")
+                                .resizable()
+                                .frame(width: 46, height: 53)
+                                .foregroundColor(Color.kStreak)
+                            
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text("16")
+                                    .font(.largeTitle.bold())
                                     .foregroundColor(Color.kStreak)
-                                VStack(alignment:.leading) {
-                                    Text("16")
-                                        .font(.largeTitle.bold())
-                                        .foregroundColor(Color.kStreak)
-                                    
-                                    Text("days streak")
-                                        .font(.system(.callout, weight: .medium))
-                                        .foregroundColor(Color.kLabel)
-                                }
-                            }
-                            
-                            RoundedRectangle(cornerRadius: 6)
-                                .foregroundColor(Color.kFrameStreak)
-                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                                .overlay(
-                                    Text("Congratulations on your 16-day streak! 🎉 Keep up the great work—consistency is key to success. Just a 30 minutes each day can make a huge difference! 🚀")
-                                        .font(.caption)
-                                        .foregroundColor(Color.black)
-                                )
-                                .frame(width: 361, height: 81)
-                        }
-                        .padding(16)
-                        .background(.white)
-                        
-                        VStack(alignment: .center, spacing: 16){
-                            HStack{
-                                Text("Badges")
-                                    .font(.system(.body, weight: .bold))
-                                Spacer()
-                                Button{
-                                    isViewClicked.toggle()
-                                } label: {
-                                    Text("View All")
-                                        .font(.system(.footnote, weight: .semibold))
-                                        .foregroundColor(Color.kLabel)
-                                }
-                            }
-                            HStack {
-                                ForEach(badgeImage.prefix(5), id: \.id){ image in
-                                    ZStack {
-                                        Image("\(image.image)")
-                                            .resizable()
-                                            .frame(width: 64, height: 75)
-                                            .opacity(image.isAchieved ? 1 : 0.3)
-                                        
-                                    }
-                                    .cornerRadius(10)
-                                }
+                                
+                                Text("days streak")
+                                    .font(.system(.callout, weight: .medium))
+                                    .foregroundColor(Color.kLabel)
                             }
                         }
-                        .padding(16)
-                        .background(.white)
                         
-                        VStack(alignment: .leading){
-                            Text("Calendar")
+                        Text("Congratulations on your 16-day streak! 🎉 Keep up the great work—consistency is key to success. Just a 30 minutes each day can make a huge difference! 🚀")
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .foregroundColor(.kTitleText)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .padding()
+                            .background(.kBackground)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    .padding(16)
+                    .background(.white)
+                    
+                    VStack(alignment: .center, spacing: 16){
+                        HStack{
+                            Text("Badges")
                                 .font(.system(.body, weight: .bold))
-                            MultiRangeDatePicker()
-                                .padding(.horizontal, 5)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.kAccent, lineWidth: 2)
-                                        .frame(maxWidth: .infinity)
-                                )
-                            
+                            Spacer()
+                            Button{
+                                isViewClicked.toggle()
+                            } label: {
+                                Text("View All")
+                                    .font(.system(.footnote, weight: .semibold))
+                                    .foregroundColor(Color.kSuccess)
+                            }
                         }
-                        .padding(16)
-                        .background(.white)
+                        HStack {
+                            ForEach(badgeImage.prefix(5), id: \.id){ image in
+                                ZStack {
+                                    Image("\(image.image)")
+                                        .resizable()
+                                        .frame(width: 64, height: 75)
+                                        .opacity(image.isAchieved ? 1 : 0.3)
+                                    
+                                }
+                                .cornerRadius(10)
+                            }
+                        }
                     }
-                    .padding(.bottom, 32)
-                    .sheet(isPresented: $isViewClicked){
-                        BadgesScreen()
+                    .padding(16)
+                    .background(.white)
+                    
+                    VStack(alignment: .leading){
+                        Text("Calendar")
+                            .font(.system(.body, weight: .bold))
+                        MultiRangeDatePicker()
+                            .padding(.horizontal, 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.kAccent, lineWidth: 2)
+                                    .frame(maxWidth: .infinity)
+                            )
+                        
                     }
+                    .padding(16)
+                    .background(.white)
                 }
-                .navigationTitle("Streak")
-                .font(.title)
-                .navigationBarTitleDisplayMode(.inline)
+                .padding(.bottom, 32)
+                .sheet(isPresented: $isViewClicked){
+                    BadgesScreen()
+                }
+              
             }
+            .navigationTitle("Streak")
+            .navigationBarTitleDisplayMode(.inline)
+            .background(.kBackground)
         }
     }
 }
 
 struct StreakScreen_Previews: PreviewProvider {
     static var previews: some View {
-        StreakScreen(isViewClicked: true)
+        StreakScreen(isViewClicked: false)
     }
 }
 
