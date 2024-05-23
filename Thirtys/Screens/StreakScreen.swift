db2db7e0dcae33824d1b21606a06e9d0f7ddd896
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-23 15:07:02 +0700

    merge

--- Thirtys/Screens/StreakScreen.swift
+++ Thirtys/Screens/StreakScreen.swift
@@ -2,17 +2,112 @@
 //  StreakScreen.swift
 //  Thirtys
 //
-//  Created by Ivan Nur Ilham Syah on 23/05/24.
+//  Created by DwikySP on 22/05/24.
 //
 
 import SwiftUI
 
 struct StreakScreen: View {
+    
+    var badgeImage : [BadgeData] = BadgeData.items
+    
+    @State var isViewClicked: Bool
+    
+    
     var body: some View {
-        Text("Streak Screen")
+        NavigationStack{
+            ScrollView{
+                VStack(alignment: .leading, spacing: 24){
+                    VStack (alignment: .leading){
+                        HStack(spacing: 4) {
+                            Image(systemName: "flame.fill")
+                                .resizable()
+                                .frame(width: 46, height: 53)
+                                .foregroundColor(Color.kStreak)
+                            
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text("16")
+                                    .font(.largeTitle.bold())
+                                    .foregroundColor(Color.kStreak)
+                                
+                                Text("days streak")
+                                    .font(.system(.callout, weight: .medium))
+                                    .foregroundColor(Color.kLabel)
+                            }
+                        }
+                        
+                        Text("Congratulations on your 16-day streak! 🎉 Keep up the great work—consistency is key to success. Just a 30 minutes each day can make a huge difference! 🚀")
+                            .font(.caption)
+                            .fontWeight(.medium)
+                            .foregroundColor(.kTitleText)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .padding()
+                            .background(.kBackground)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    .padding(16)
+                    .background(.white)
+                    
+                    VStack(alignment: .center, spacing: 16){
+                        HStack{
+                            Text("Badges")
+                                .font(.system(.body, weight: .bold))
+                            Spacer()
+                            Button{
+                                isViewClicked.toggle()
+                            } label: {
+                                Text("View All")
+                                    .font(.system(.footnote, weight: .semibold))
+                                    .foregroundColor(Color.kSuccess)
+                            }
+                        }
+                        HStack {
+                            ForEach(badgeImage.prefix(5), id: \.id){ image in
+                                ZStack {
+                                    Image("\(image.image)")
+                                        .resizable()
+                                        .frame(width: 64, height: 75)
+                                        .opacity(image.isAchieved ? 1 : 0.3)
+                                    
+                                }
+                                .cornerRadius(10)
+                            }
+                        }
+                    }
+                    .padding(16)
+                    .background(.white)
+                    
+                    VStack(alignment: .leading){
+                        Text("Calendar")
+                            .font(.system(.body, weight: .bold))
+                        MultiRangeDatePicker()
+                            .padding(.horizontal, 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.kAccent, lineWidth: 2)
+                                    .frame(maxWidth: .infinity)
+                            )
+                        
+                    }
+                    .padding(16)
+                    .background(.white)
+                }
+                .padding(.bottom, 32)
+                .sheet(isPresented: $isViewClicked){
+                    BadgesScreen()
+                }
+              
+            }
+            .navigationTitle("Streak")
+            .navigationBarTitleDisplayMode(.inline)
+            .background(.kBackground)
+        }
     }
 }
 
-#Preview {
-    StreakScreen()
+struct StreakScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        StreakScreen(isViewClicked: false)
+    }
 }
+
--- Thirtys/Shared/Components/MultiRangeDatePicker.swift
+++ Thirtys/Shared/Components/MultiRangeDatePicker.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MultiRangeDatePicker: View {
     
-    @State private var isConfirmationSheetOpen: Bool = false
+    @State private var isConfirmationSheetOpen: Bool = true
     @State private var dates: Set<DateComponents> = []
     @State private var coba: String = "Choose your range schedule"
     var uiColor = UIColor(red: 0.52, green: 0.86, blue: 0.78, alpha: 1.00)
@@ -18,76 +18,15 @@ struct MultiRangeDatePicker: View {
     
     
     var body: some View {
-        HStack {
-            Text("Select dates:")
-            Spacer()
-            Button {
-                isConfirmationSheetOpen.toggle()
-            } label: {
-                Text(coba)
-            }
-        }
-        .datePickerStyle(.compact)
-        .offset(y: 40)
-        .padding(20)
-        .sheet(isPresented: $isConfirmationSheetOpen) {
-            Spacer()
-            VStack(alignment: .leading){
-                Text("Plan Duration")
-                    .foregroundColor(Color(uiColor: uiColor))
-                    .bold()
-                    .font(.subheadline)
-                    .padding(.leading, 10)
-
-                    
-                MultiDatePicker("Dates", selection: $dates, in: Date()...)
-                    
-                    .datePickerStyle(.compact)
-                    .presentationDetents([.medium])
-                    .padding(.vertical, -10)
-                    .labelsHidden()
-                    .tint(Color(uiColor: titleColor))
-                    .preferredColorScheme(.light)
-            }
-            .frame(maxWidth: .infinity,alignment: .leading)
+        VStack(alignment: .leading){
+            MultiDatePicker("Dates", selection: $dates, in: Date()...)
             
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color(uiColor: uiColor), lineWidth: 2)
-            )
-            .accentColor(Color(uiColor: uiColor))
-            .padding()
-            
+                .datePickerStyle(.graphical)
+                .tint(Color(uiColor: titleColor))
+                .clipped()
         }
-        .onChange(of: dates) { oldDates, newDates in
-            if newDates.count == 2 {
-                coba = getDateInterval(date: newDates)
-                isConfirmationSheetOpen.toggle()
-            } else if newDates.count > 2 {
-                dates = []
-                coba = "Choose your range schedule"
-            }
-        }
+        
     }
-}
-
-func getDateInterval(date: Set<DateComponents>) -> String {
-    let datesArr: [Date] = date.compactMap { Calendar.current.date(from: $0) }.sorted()
-    
-    if datesArr.count == 2 {
-        let startDate = datesArr[0]
-        let endDate = datesArr[1]
-        
-//        let monthDifference = Calendar.current.dateComponents([.month], from: startDate, to: endDate).month ?? 0
-//        let weekDifference = Calendar.current.dateComponents([.weekOfYear], from: startDate, to: endDate).weekOfYear ?? 0
-        
-        // \n\(monthDifference) month(s) and \(weekDifference) week(s)
-        
-        return "\(startDate.formatted(date: .abbreviated, time: .omitted)) - \(endDate.formatted(date: .abbreviated, time: .omitted))"
-    }
-    
-    return "none"
 }
 
 #Preview {
