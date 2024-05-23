9d25df05394f7ad3b2bbcd136ea09c764a1a9f8b
Bayu Septyan <bayuseptyan925@gmail.com>
2024-05-23 12:01:55 +0700

    feat: logic home screen

--- Thirtys/Shared/Components/BadgeComponent.swift
+++ Thirtys/Shared/Components/BadgeComponent.swift
@@ -13,18 +13,20 @@ struct BadgeComponent: View {
     var title: String
     var desc: String
     
+    @Binding var showAchievementPopup: Bool
+    
     var body: some View {
         VStack(spacing: 32) {
-//            Button(action: {
-////                isPopUpOpen.toggle()
-//            }, label: {
-//                Image(systemName: "xmark.circle")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(width: 32, height: 32)
-//            })
-//            .foregroundStyle(.gray)
-//            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+            Button(action: {
+                showAchievementPopup.toggle()
+            }, label: {
+                Image(systemName: "xmark.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+            })
+            .foregroundStyle(.gray)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
             
             Text("You Got New Badge!")
                 .font(.title2)
@@ -45,11 +47,22 @@ struct BadgeComponent: View {
         }
         .padding(.vertical, 25)
         .padding(.horizontal, 50)
-        .background()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(BlurView())
         .cornerRadius(15)
+    }
+}
+struct BlurView : UIViewRepresentable {
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterialLight))
+        return view
     }
 }
 
 #Preview {
-    BadgeComponent(image: "1st-learning", title: "Congratulations! 🎉", desc: "You’ve completed your first learning session! You're off to a great start!")
+    BadgeComponent(image: "1st-learning", title: "Congratulations! 🎉", desc: "You’ve completed your first learning session! You're off to a great start!", showAchievementPopup: .constant(true))
 }
