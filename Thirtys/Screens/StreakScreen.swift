7f16853cf77d768304dde51186692823ef8b203a
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-23 13:09:49 +0700

    feat(componetn): add custom background and foreground color constructor to app button style

---
+++ Thirtys/Screens/StreakScreen.swift
@@ -0,0 +1,18 @@
+//
+//  StreakScreen.swift
+//  Thirtys
+//
+//  Created by Ivan Nur Ilham Syah on 23/05/24.
+//
+
+import SwiftUI
+
+struct StreakScreen: View {
+    var body: some View {
+        Text("Streak Screen")
+    }
+}
+
+#Preview {
+    StreakScreen()
+}
--- Thirtys/Screens/TodayScreen.swift
+++ Thirtys/Screens/TodayScreen.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 
-struct HomeScreen: View {
+struct TodayScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }
     }
 }
 
 #Preview {
-    HomeScreen()
+    TodayScreen()
 }
