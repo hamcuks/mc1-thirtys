a4180526f7b3ddcfdac0ec9c545e9b7a5a3721b2
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-24 20:28:17 +0700

    feat(vm): implement plan and user pref service in goal vm

--- Thirtys/Shared/ViewModels/GoalViewModel.swift
+++ Thirtys/Shared/ViewModels/GoalViewModel.swift
@@ -10,34 +10,22 @@ import SwiftUI
 import CoreData
 
 class GoalViewModel: ObservableObject {
-    var database: PersistenceController = PersistenceController.shared
+    private let planService: PlanService = PlanService.shared
+    private let userPrefService: UserPreferenceService = UserPreferenceService.shared
     
     @Published var plan: PlanEntity? = nil
     @Published var learningTimes: [GrouppedWeekdayEvent] = []
     
     func getPlanData() {
-        // Create NSFetchRequest from PlanEntity
-        let request = NSFetchRequest<PlanEntity>(entityName: "PlanEntity")
-        
-        do {
-            // Get list of data based on NSFetchRequest
-            let plans = try database.container.viewContext.fetch(request)
-            
-            // Select last plan data
-            if let plan = plans.last {
-                self.plan = plan
-                
-                self.getLearningTimes()
-            }
-        } catch {
-            print("Error: \(error.localizedDescription)")
-        }
+        self.plan = planService.getAll().last
     }
     
-    private func getLearningTimes() {
+    func getLearningTimes() {
         
         // Convert learningTimes object to Array
-        let items = (self.plan?.learningTimes?.allObjects as? Array<LearningTimeEntity> ?? [])
+        let items = userPrefService.getLearningTimes()
+        
+        print("learning time: \(items)")
         
         // Get the learning time weekday
         let weekday: [Int16] = Set(items.map { $0.day }).sorted()
