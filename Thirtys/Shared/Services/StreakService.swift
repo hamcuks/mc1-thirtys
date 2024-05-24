d85566184d8b794709368ed4c41d76d091811452
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-24 20:45:31 +0700

    feat(service): create streak service

--- Thirtys/Shared/Services/StreakService.swift
+++ Thirtys/Shared/Services/StreakService.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import CoreData
+
+class StreakService: ObservableObject {
+    private let database: NSManagedObjectContext = PersistenceController.shared.container.viewContext
+    static let shared: StreakService = StreakService()
+    
+    func getBadges(planId: UUID) -> [BadgeData] {
+        return []
+    }
+    
+    func getLearningStreaks() -> [StreakEntity] {
+        let request: NSFetchRequest = NSFetchRequest<StreakEntity>(entityName: "StreakEntity")
+        
+        do {
+            let items = try database.fetch(request)
+            
+            return items
+        } catch {
+            print("Error: \(error.localizedDescription)")
+            
+            return []
+        }
+    }
+    
+    func updateDailyStreak(plan: PlanEntity, learningHistory: [Event]) {
+        let updatePlan = plan
+        
+        let streak = StreakEntity(context: database)
+        streak.date = .now
+        streak.history = NSSet(
+            array: learningHistory.map { history in
+                let learnHistory = LearningHistoryEntity(context: database)
+                learnHistory.startTime = history.startTime
+                learnHistory.endTime = history.endTime
+                
+                return learnHistory
+            }
+        )
+        
+        updatePlan.streaks = NSSet(array: [streak])
+        
+        do {
+            try database.save()
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func collectBadge(badge: BadgeData) {
+        
+    }
+}
