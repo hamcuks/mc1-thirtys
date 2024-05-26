c2b40e603f25c9f5e221009d14fed8d1d7a373f8
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-26 11:40:36 +0700

    feat: create timer live activity

--- LearningTimer/LearningTimerLiveActivity.swift
+++ LearningTimer/LearningTimerLiveActivity.swift
@@ -9,72 +9,129 @@ import ActivityKit
 import WidgetKit
 import SwiftUI
 
-struct LearningTimerAttributes: ActivityAttributes {
-    public struct ContentState: Codable, Hashable {
-        // Dynamic stateful properties about your activity go here!
-        var emoji: String
-    }
-
-    // Fixed non-changing properties about your activity go here!
-    var name: String
-}
-
 struct LearningTimerLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: LearningTimerAttributes.self) { context in
             // Lock screen/banner UI goes here
             VStack {
-                Text("Hello \(context.state.emoji)")
+//                Text("Hello \(context.state.emoji)")
             }
             .activityBackgroundTint(Color.cyan)
             .activitySystemActionForegroundColor(Color.black)
-
+            
         } dynamicIsland: { context in
             DynamicIsland {
-                // Expanded UI goes here.  Compose the expanded UI through
-                // various regions, like leading/trailing/center/bottom
-                DynamicIslandExpandedRegion(.leading) {
-                    Text("Leading")
+                DynamicIslandExpandedRegion(.leading, priority: 2) {
+                    HStack {
+                        Image(systemName: "pause.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(14)
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.orange)
+                            .background(.orange.opacity(0.2))
+                            .clipShape(Circle())
+                        
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(14)
+                            .bold()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.white)
+                            .background(.white.opacity(0.2))
+                            .clipShape(Circle())
+                    }
                 }
-                DynamicIslandExpandedRegion(.trailing) {
-                    Text("Trailing")
+                DynamicIslandExpandedRegion(.center) {
+                    
+                        Text("Remaining")
+                            .font(.subheadline)
+                            .foregroundStyle(.kSuccess)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 48, alignment: .trailing)
                 }
-                DynamicIslandExpandedRegion(.bottom) {
-                    Text("Bottom \(context.state.emoji)")
-                    // more content
+                DynamicIslandExpandedRegion(.trailing, priority: 0) {
+                    
+                    Text("00:00")
+                    .hidden()
+                    .overlay {
+                        Text(
+                            context.state.timer,
+                            style: .timer
+                        )
+                        .bold()
+                    }
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.kSuccess)
+                    .frame(height: 48, alignment: .bottom)
                 }
+                
             } compactLeading: {
-                Text("L")
+                ProgressView(
+                    timerInterval: context.state.startDate...context.state.timer,
+                    countsDown: true,
+                    label: { EmptyView() }
+                ) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 10)
+                        .foregroundStyle(.kSuccess)
+                }
+                .labelsHidden()
+                .progressViewStyle(.circular)
+                .frame(height: 24)
+                .tint(.kSuccess)
+                .padding(.trailing, 24)
+                
             } compactTrailing: {
-                Text("T \(context.state.emoji)")
+                Text("00:00")
+                    .hidden()
+                    .overlay {
+                        Text(
+                            context.state.timer,
+                            style: .timer
+                        )
+                    }
+                    .foregroundStyle(.kSuccess)
+                    .bold()
+                
             } minimal: {
-                Text(context.state.emoji)
+                ProgressView(
+                    timerInterval: context.state.startDate...context.state.timer,
+                    countsDown: true,
+                    label: { EmptyView() }
+                ) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 10)
+                        .foregroundStyle(.kSuccess)
+                }
+                .labelsHidden()
+                .progressViewStyle(.circular)
+                .frame(height: 24)
+                .tint(.kSuccess)
             }
-            .widgetURL(URL(string: "http://www.apple.com"))
-            .keylineTint(Color.red)
+            .keylineTint(.kAccent)
         }
     }
 }
 
 extension LearningTimerAttributes {
     fileprivate static var preview: LearningTimerAttributes {
-        LearningTimerAttributes(name: "World")
+        LearningTimerAttributes()
     }
 }
 
 extension LearningTimerAttributes.ContentState {
     fileprivate static var smiley: LearningTimerAttributes.ContentState {
-        LearningTimerAttributes.ContentState(emoji: "😀")
-     }
-     
-     fileprivate static var starEyes: LearningTimerAttributes.ContentState {
-         LearningTimerAttributes.ContentState(emoji: "🤩")
-     }
+        LearningTimerAttributes.ContentState(startDate: .now, timer: .now)
+    }
 }
 
-#Preview("Notification", as: .content, using: LearningTimerAttributes.preview) {
-   LearningTimerLiveActivity()
+#Preview("Notification", as: .dynamicIsland(.compact), using: LearningTimerAttributes.preview) {
+    LearningTimerLiveActivity()
 } contentStates: {
     LearningTimerAttributes.ContentState.smiley
-    LearningTimerAttributes.ContentState.starEyes
 }
