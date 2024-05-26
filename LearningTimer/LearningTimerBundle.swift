2e401058c0a7aa07c0bc1fb1b1b71454c8e5d186
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-26 11:40:04 +0700

    feat: add live activity timer support

---
+++ LearningTimer/LearningTimerBundle.swift
@@ -0,0 +1,17 @@
+//
+//  LearningTimerBundle.swift
+//  LearningTimer
+//
+//  Created by Ivan Nur Ilham Syah on 26/05/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LearningTimerBundle: WidgetBundle {
+    var body: some Widget {
+        LearningTimer()
+        LearningTimerLiveActivity()
+    }
+}
---
+++ LearningTimer/Models/LearningTimerAttributes.swift
@@ -0,0 +1,18 @@
+//
+//  LearningTimerAttributes.swift
+//  Thirtys
+//
+//  Created by Ivan Nur Ilham Syah on 26/05/24.
+//
+
+import Foundation
+import ActivityKit
+
+struct LearningTimerAttributes: ActivityAttributes {
+    public typealias TimerStatus = ContentState
+    
+    public struct ContentState: Codable, Hashable {
+        var startDate: Date
+        var timer: Date
+    }
+}
